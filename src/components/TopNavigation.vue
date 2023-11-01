ffa7251f22ab037547696bdc429bafaaebc5a8ea
Quincy Tromp <quincy.tromp@hotmail.com>
2023-11-01 13:34:00 +0100

    Added TopNavigation component

---
+++ src/components/TopNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="topnav">
+        <router-link to="/">HappyPaw</router-link> 
+        <router-link to="/contact">Contact</router-link> 
+        <router-link to="/afspraak-maken">Afspraak</router-link>
+    </div>
+</template>
+
+<style>
+    .topnav {
+        background-color: #ffbd54;
+        overflow: hidden;
+    }
+
+    .topnav a {
+        float: left;
+        color: #ffffff;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+    }
+
+    .topnav a:hover {
+        background-color: #ddd;
+        color: black;
+    }
+</style>
