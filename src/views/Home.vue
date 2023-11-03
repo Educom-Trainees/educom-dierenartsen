a7949e8017ca61c04d63be44c7075e95e4c2615d
Stijn121 <s.engelmoer@ziggo.nl>
2023-11-03 15:55:06 +0100

    een paar designs + begin homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    <h1>Home pagina:</h1>
+    <h1>Topzorg voor uw huisdier, dat krijgt u bij ons!</h1>
+    <h2>Uw huisdier is bij ons in goede handen</h2>
+    <p>
+    Als dierenarts dragen wij de verantwoordelijkheid voor de gezondheid van uw huisdier. 
+    Wij willen er samen met u voor zorgen dat uw hond, kat, konijn of welk ander huisdier dan ook in optimale gezondheid van zijn of haar leven kan genieten. 
+    Om daarvoor te zorgen zetten wij alles op alles. 
+    Onze kliniek heeft twee ervaren dierenartsen die uw huisdier optimaal kunnen verzorgen. 
+    Van het schoonmaken van de tanden of het knippen van de nagels tot het castreren of steriliseren van uw huisdier. 
+    Maak gerust een afspraak, dan kunnen we de beste zorg bieden voor uw huisdier</p>
+    <p>U bent van harte welkom,</p>
+    <p>Tot ziens in Dierenkliniek HappyPaws Zeist</p>
   </div>
 </template>
 
@@ -12,3 +22,11 @@ export default {
   }
 }
 </script>
+
+<style>
+p {
+  max-width: 50%;
+  text-align: center;
+  margin-left: 25%;
+}
+</style>
