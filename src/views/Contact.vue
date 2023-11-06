eeb9cee7f6df6147914f2b06b2e66280be26a316
Quincy Tromp <quincy.tromp@hotmail.com>
2023-11-06 16:16:54 +0100

    Updated Contact page styling

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="contact">
-    <h1>Neem contact op met onze kliniek</h1>
-    <p>We vinden het belangrijk om bereikbaar te zijn voor onze klanten, 
-        hieronder vind u de belangrijke informatie om ons te bereiken:</p>
-    <p>U bent van harte welkom,</p>
-    <p> Bezoek adres (op afspraak): Fazantenplein 3, 3704 EV, Zeist</p>
-    <p> Telefoonnummer:  030-39-81-984 (u kunt hier ook een afspraak inplannen) </p>
-    <p> Noodnummer:  06-84-49-28-33 (alleen voor spoedgevallen!)</p>
-    <p> E-mail: info@happypaws.nl</p>
+  <div class="container">
+    <div class="row">
+        <div class="col-10 col-md-8">
+            <h1 class="text-lg blue">Neem contact op met onze kliniek</h1>
+            <h2 class="text-md blue-l mb-4">
+              We vinden het belangrijk om bereikbaar te zijn voor onze klanten, 
+              hieronder vind u de belangrijke informatie om ons te bereiken
+            </h2>
+            <address>
+              <strong>Bezoek adres</strong> <small>(op afspraak)</small><br>
+              Fazantenplein 3<br>
+              3704 EV<br>
+              Zeist<br>
+            </address>
+            <address>
+              <strong>Telefoonnummer</strong> <small>(u kunt hier ook een afspraak inplannen)</small><br>
+              030-39-81-984
+            </address>
+            <address>
+              <strong>Noodnummer</strong> <small>(alleen voor spoedgevallen!)</small><br>
+              06-84-49-28-33
+            </address>
+            <address>
+              <strong>E-mail</strong><br>
+              <a href="mailto:#">info@happypaws.nl</a>
+            </address>
+        </div>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'Home',
+  }
+</script>
+
+<style>
+  .container * {
+    text-align: left;
+  }
+  .container {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+</style>
