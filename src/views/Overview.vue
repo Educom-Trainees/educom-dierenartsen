cf5327c2fd5dc700774725b1156bb53fb91a0d2d
Quincy Tromp <quincy.tromp@hotmail.com>
2023-11-06 13:34:43 +0100

    Initial commit Overview page

---
+++ src/views/Overview.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-md-8">
+            <h1 class="text-lg">Overzicht afspraken</h1>
+            <table class="table table-bordered">
+                <tr>
+                    <th><i class="fa fa-clock-o" aria-hidden="true"></i></th>
+                    <th>Karel</th>
+                    <th>Danique</th>
+                </tr>
+                <tr>
+                    <td>9:00</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+
+</style>
