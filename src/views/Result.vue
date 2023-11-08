5404b386c8200cea890e30e728eaaabcbfffdfec
Stijn121 <s.engelmoer@ziggo.nl>
2023-11-08 15:20:01 +0100

    result verbeterd

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,18 +1,19 @@
 <template>
-  <h1>Afspraak:</h1>
-  <h3>afspraak nummer: {{ appointment.number }}</h3>
-  <h3>naam: {{ appointment.customer }}</h3>
-  <h3>email: {{ appointment.email }}</h3>
-  <h3>telefoonnummer: {{ appointment.phoneNumber }}</h3>
-  <h3>datum: {{ appointment.date }}</h3>
-  <h3>tijd: {{ appointment.time }}</h3>
-  <h3 v-if="appointment.doctor == 1">dokter: karel lant</h3>
-  <h3 v-if="appointment.doctor == 2">dokter: danique de beer</h3>
-  <p>we hopen u dan te zien (: </p>
+  <div v-if="appointment" class="appointment">
+    <h1>Afspraak:</h1>
+    <h3>afspraak nummer: {{ appointment.number }}</h3>
+    <h3>naam: {{ appointment.customer }}</h3>
+    <h3>email: {{ appointment.email }}</h3>
+    <h3>telefoonnummer: {{ appointment.phoneNumber }}</h3>
+    <h3>datum: {{ appointment.date }}</h3>
+    <h3>tijd: {{ appointment.time }}</h3>
+    <h3 v-if="appointment.doctor == 1">dokter: karel lant</h3>
+    <h3 v-if="appointment.doctor == 2">dokter: danique de beer</h3>
+    <p>we hopen u dan te zien (: </p>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
 import getAppointment from '../composables/getAppointment'
 export default {
   props: ['id'],
@@ -23,15 +24,6 @@ export default {
 
     return { appointment, error }
   }
-  // setup() {
-
-  //   const showAppointments = ref(true)
-  //   return {
-  //     appointments,
-  //     error,
-  //     showAppointments
-  //   }
-  // }
 }
 </script>
 
