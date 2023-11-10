c9af617a76c3c98cd0d4d7513ba367f97176f54b
Stijn121 <s.engelmoer@ziggo.nl>
2023-11-10 16:47:04 +0100

    verbetering afpraak maken frontend

--- src/views/Appointment.vue
+++ src/views/Appointment.vue
@@ -1,111 +1,130 @@
 <template>
   <div class="planning">
-    <h1>Afspraak maken:</h1>
-    <form @submit.prevent="handleSubmit" v-if="showForm"> 
-      <!-- <label>Naam: </label>
-      <input type="text" required v-model="name"><br>
-      <div v-if="nameError" class="error">{{ nameError }}</div>
-      <label>Email: </label>
-      <input type="email" required v-model="email"><br>
-      <div v-if="emailError" class="error">{{ emailError }}</div>
-      <label>Telefoonnummer: </label>
-      <input type="text" required v-model="phone"><br>
-      <div v-if="phoneError" class="error">{{ phoneError }}</div> -->
-      <label>type dier: </label>
-        <select required v-model="type_animal">
-          <option value="1">hond</option>
-          <option value="8">kleine hond</option>
-          <option value="9">grote hond</option>
-          <option value="2">kat</option>
-          <option value="3">konijn</option>
-          <option value="4">cavia</option>
-          <option value="5">hamster</option>
-          <option value="6">rat</option>
-          <option value="7">muis</option>
-        </select><br>
-      <label>aantal: </label>
-        <select v-model="amount">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-        </select><br>
-      <label v-if="amount == 1">Naam huisdier: </label>
-      <label v-if="amount != 1">Naam eerste huisdier: </label>
-      <input type="text" v-model="name_animal[0]"><br>
-      <div v-if="amount != 1">
-        <label>Naam tweede huisdier: </label>
-        <input type="text" v-model="name_animal[1]">
-      </div>
-      <div v-if="amount != 1 && amount != 2">
-        <label>Naam derde huisdier: </label>
-        <input type="text" v-model="name_animal[2]">
-      </div>
-      <div v-if="amount == 4">
-        <label>Naam vierde huisdier: </label>
-        <input type="text" v-model="name_animal[3]">
-      </div>
-      <label>type consult: </label>
-        <select required v-model="type_consult">
-          <option value="1">consult</option>
-          <option value="2">eerste consult</option>
-          <option value="3">vaccinatie</option>
-          <option value="4">anaal klieren legen</option>
-          <option value="5">nagels knippen</option>
-          <option value="6">bloed onderzoek</option>
-          <option value="7">urine onderzoek</option>
-          <option value="8">gebitscontrole</option>
-          <option value="9">postoperatieve controle</option>
-          <option value="10">herhaal recept bestellen</option>
-        </select>
-        <div class="submit">
-          <button>volgende</button>
+    <h1 class="appointment_header">Afspraak maken</h1>
+    <div class="test" v-if="showForm">
+      <h2>hier komt de balk</h2>
+      <form @submit.prevent="handleSubmit" v-if="showForm" class="appointment_form">
+        <!-- <div class="test"> -->
+        <h4 class="appointment_header_2">Afspraak voorkeuren</h4>
+        <label>Selecteer diersoort* </label><br>
+          <!-- <select>
+            <option value="8">kleine hond</option>
+            <option value="1">gemiddelde hond</option>
+            <option value="9">grote hond</option>
+          </select> -->
+          <button @click="changetype_animal(1)" type="button" id="big" value="1" :class="{selected_animal: type_animal == 1}"><img src="../assets/dog.png"><br>hond</button>
+          <button @click="changetype_animal(2)" type="button" id="big" value="2" :class="{selected_animal: type_animal == 2}"><img src="../assets/black-cat.png"><br>kat</button>
+          <button @click="changetype_animal(3)" type="button" id="big" value="3" :class="{selected_animal: type_animal == 3}"><img src="../assets/rabbit.png"><br>konijn</button>
+          <button @click="changetype_animal(4)" type="button" id="big" value="4" :class="{selected_animal: type_animal == 4}"><img src="../assets/guinea-pig.png"><br>cavia</button>
+          <button @click="changetype_animal(5)" type="button" id="big" value="5" :class="{selected_animal: type_animal == 5}"><img src="../assets/hamster.png"><br>hamster</button>
+          <button @click="changetype_animal(6)" type="button" id="big" value="6" :class="{selected_animal: type_animal == 6}"><img src="../assets/rat.png"><br>rat</button>
+          <button @click="changetype_animal(7)" type="button" id="big" value="7" :class="{selected_animal: type_animal == 7}"><img src="../assets/muis.png"><br>muis</button>
+          <!-- <button @click="changetype_animal(8)" type="button" id="big" value="8" :class="{selected_animal: type_animal == 8}">kleine hond</button>
+          <button @click="changetype_animal(9)" type="button" id="big" value="9" :class="{selected_animal: type_animal == 9}">grote hond</button> -->
+          <br>
+        <label>Aantal huisdieren* </label><br>
+          <button @click="changeamount(1)" type="button" value="1" id="small" :class="{selected: amount == 1}">1</button>
+          <button @click="changeamount(2)" type="button" value="2" id="small" :class="{selected: amount == 2}">2</button>
+          <button @click="changeamount(3)" type="button" value="3" id="small" :class="{selected: amount == 3}">3</button>
+          <button @click="changeamount(4)" type="button" value="4" id="small" :class="{selected: amount == 4}">4</button>
+        <br>
+        <label v-if="amount == 1">Naam huisdier </label>
+        <label v-if="amount != 1">Naam eerste huisdier </label>
+        <input type="text" v-model="name_animal[0]"><br>
+        <div v-if="amount != 1">
+          <label>Naam tweede huisdier </label>
+          <input type="text" v-model="name_animal[1]">
         </div>
+        <div v-if="amount != 1 && amount != 2">
+          <label>Naam derde huisdier </label>
+          <input type="text" v-model="name_animal[2]">
+        </div>
+        <div v-if="amount == 4">
+          <label>Naam vierde huisdier </label>
+          <input type="text" v-model="name_animal[3]">
+        </div>
+        <div v-if="name_animalError" class="error">{{ name_animalError }}</div>
+          <select required v-model="type_consult" id="type_select">
+            <option hidden value="">Selecteer type afspraak*</option>
+            <option value="1">consult</option>
+            <option value="2">eerste consult</option>
+            <option value="3">vaccinatie</option>
+            <option value="4">anaal klieren legen</option>
+            <option value="5">nagels knippen</option>
+            <option value="6">bloed onderzoek</option>
+            <option value="7">urine onderzoek</option>
+            <option value="8">gebitscontrole</option>
+            <option value="9">postoperatieve controle</option>
+            <option value="10">herhaal recept bestellen</option>
+          </select><br>
+            <button class="back">vorige</button>
+            <button class="submit">volgende</button>
+        <!-- </div> -->
     </form>
-
-    <form @submit.prevent="handledateSubmit" v-if="showdateForm">
-      <label>voorkeur: </label>
-        <select v-model="preference">
-          <option value="1">karel lant</option>
-          <option value="2">danique de beer</option>
-          <option value="0">geen voorkeur</option>
-        </select><br>
-      <label>Datum: </label>
-        <input type="date" required v-model="date">
-      <label>Tijd:</label>
+    
+    </div>
+    <div class="test" v-if="showForm">
+      <h2>test</h2>
+    </div>
+    <div class="test" v-if="showdateForm">
+    <h2>hier komt de balk</h2>
+    <form @submit.prevent="handledateSubmit" v-if="showdateForm" class="appointment_form">
+      <label>Clinici</label><br>
+        <button @click="changepreference(0)" type="button" value="0" id="block" :class="{selectedblock: preference == 0}">geen voorkeur</button>
+        <button @click="changepreference(1)" type="button" value="1" id="block" :class="{selectedblock: preference == 1}">karel lant</button>
+        <button @click="changepreference(2)" type="button" value="2" id="block" :class="{selectedblock: preference == 2}">danique de beer</button><br>
+      <label>Datum: </label><br>
+        <input type="date" required v-model="date"><br>
+      <label>Tijd:</label><br>
         <div v-if="time_slots.length">
-              <select required v-model="timeslotdata">
-              <option :value="timeslot" v-for="timeslot in time_slots" :key="timeslot.time">
-                <div v-if="timeslot.doctor == this.preference || this.preference == 0">
-                  tijd: {{ timeslot.time }} dokter: {{ timeslot.doctor }}
-                </div>
-              </option>
-              <!-- <p v-if="timeslot.doctor == this.preference || this.preference == 0">
-                tijd: {{ timeslot.time }} dokter: {{ timeslot.doctor }}
-              </p> -->
-              </select>
+            <select required v-model="timeslotdata" v-if="this.preference == 0">
+                <option :value="timeslot" v-for="timeslot in time_slots" :key="timeslot.time">
+                  <div v-if="timeslot.doctor == 1">
+                    tijd: {{ timeslot.time }} dokter: karel lant
+                  </div>
+                  <div v-if="timeslot.doctor == 2">
+                    tijd: {{ timeslot.time }} dokter: danique de beer
+                  </div>
+                </option>
+            </select>
+            <select required v-model="timeslotdata" v-if="this.preference == 1">
+                <option :value="timeslot" v-for="timeslot in time_slots.filter(t => t.doctor == 1)" :key="timeslot.time">
+                    tijd: {{ timeslot.time }} dokter: karel lant
+                </option>
+            </select>
+            <select required v-model="timeslotdata" v-if="this.preference == 2">
+                <option :value="timeslot" v-for="timeslot in time_slots.filter(t => t.doctor == 2)" :key="timeslot.time">
+                    tijd: {{ timeslot.time }} dokter: danique de beer
+                </option>
+            </select>
         </div>
-      <button>vorige</button>
-      <div class="submit">
-        <button>volgende</button>
-      </div>
+      <button class="back">vorige</button>
+      <button class="submit">volgende</button>
     </form>
-
-    <form @submit.prevent="Result" v-if="showcontactForm">
-      <label>Naam: </label>
-      <input type="text" required v-model="name"><br>
-      <div v-if="nameError" class="error">{{ nameError }}</div>
-      <label>Email: </label>
-      <input type="email" required v-model="email"><br>
+    </div>
+    <div class="test" v-if="showdateForm">
+      <h2>test</h2>
+    </div>
+    <div class="test" v-if="showcontactForm">
+      <h2>hier komt de balk</h2>
+      <h3>{{ date }} {{ time }}</h3>
+    <form @submit.prevent="Result" v-if="showcontactForm" class="appointment_form">
+      <h4>Persoonlijke gegevens</h4>
+      <label>Uw contactgegevens</label><br>
+      <input type="email" required v-model="email" placeholder="Email adres*"><br>
       <div v-if="emailError" class="error">{{ emailError }}</div>
-      <label>Telefoonnummer: </label>
-      <input type="text" required v-model="phone"><br>
+      <input type="text" required v-model="phone" placeholder="Mobiel nummer*"><br>
       <div v-if="phoneError" class="error">{{ phoneError }}</div>
-      <button>vorige</button>
-      <div class="submit">
-        <button>bevestig afspraak</button>
-      </div>
+      <label>Uw naam</label><br>
+      <input type="text" required v-model="name" placeholder="Naam*"><br>
+      <div v-if="nameError" class="error">{{ nameError }}</div>
+      <button class="back">vorige</button>
+      <button class="submit">bevestig afspraak</button>
     </form>
+  </div>
+  </div>
+  <div class="test" v-if="showcontactForm">
+    <h2>test</h2>
   </div>
 </template>
 
@@ -114,6 +133,8 @@
   import getAppointments from '../composables/getAppointments'
   import getAppointment_type from '../composables/getAppointment_type'
   import postAppointments from '../composables/postAppointments'
+
+
 
   export default {
     name: 'app',
@@ -140,6 +161,7 @@
             time: '',
             doctor: '',
             date: '',
+            name_animalError: '',
             nameError: '',
             emailError: '',
             phoneError: '',
@@ -147,7 +169,31 @@
         }
     },
     methods: {
+      changeamount(amount){
+        this.amount = amount
+      },
+      changetype_animal(animal){
+        this.type_animal = animal
+      },
+      changepreference(preference){
+        this.preference = preference
+      },
       async handleSubmit() {
+        // for (let i = 0; i < 4;) { 
+        //   console.log(this.name_animal[i].length)
+        //   this.name_animalError = this.name_animal[i].length < 90 ?
+        //   '' : 'je huisdiernaam mag niet langer zijn dan 90 letters'
+        //   i++
+        // }
+
+        // console.log(this.name_animal[0].length)
+        // if(this.name_animalError){
+        //   return {
+        //     name_animalError
+        //   }
+        // }
+
+
         const { appointment_type, error, load } = getAppointment_type(this.type_consult)
         await load()
         for(let i=0; i < appointment_type.value.calculation.length; i++){
@@ -200,11 +246,13 @@
         // '' : 'je telefoonnummer moet bestaan uit 10 nummers'
 
         if(!this.nameError && !this.emailError){
-          console.log('je zit goed')
           const lastappointment = this.appointments[this.appointments.length - 1]
           this.number = lastappointment.id
           this.number++
-          await postAppointments(this.number, this.date, this.time, this.duration, this.name, this.phone, this.email, this.type_animal, this.type_consult, this.name_animal, this.preference, this.doctor, this.status)
+
+          await postAppointments(this.number, this.date, this.time, this.duration, this.name, this.phone, 
+          this.email, this.type_animal, this.type_consult, this.name_animal, this.preference, this.doctor, this.status)
+
           console.log('de afspraak is gepost')
           this.$router.push('/result/' + this.number)
         }
@@ -217,3 +265,122 @@
     }
   }
 </script>
+<style>
+.appointment_header{
+  text-align: left;
+  margin-left: 50px;
+}
+.appointment_header_2{
+  text-align: left;
+  margin-left: 10px;
+}
+.appointment_form{
+  text-align: left;
+  background-color: whitesmoke;
+  /* margin-left: 50px; */
+}
+.appointment_result {
+  background-color: white;
+  flex: 25%;
+}
+.appointment_result_pic {
+  background-color: white;
+  flex: 50%;
+}
+.row {
+  display: flex;
+}
+.submit {
+  float: right;
+  border-radius: 12px;
+  width: 150px;
+  background-color: blue;
+  color: white;
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+.back {
+  float: left;
+  border-radius: 12px;
+  width: 150px;
+  background-color: blue;
+  color: white;
+  margin-left: 10px;
+  margin-bottom: 10px;
+}
+.test {
+  margin-left: 50px;
+  background-color: lightgrey;
+  margin-right: 200px;
+}
+select{
+  /* float: left; */
+  background-color: white;
+  border-radius: 12px;
+}
+button.selected {
+  color: white;
+  background-color: blue;
+  border-radius: 12px;
+  width: 50px;
+  margin-right: 5px;
+  margin-left: 10px;
+}
+button.selectedblock {
+  color: white;
+  background-color: blue;
+  height: 40px;
+  width: 150px;
+  margin-left: 40px;
+}
+button.selected_animal {
+  color: white;
+  background-color: blue;
+  border-radius: 12px;
+  height: 70px;
+  width: 90px;
+  margin-right: 5px;
+  margin-left: 10px;
+}
+#small {
+  border-radius: 12px;
+  width: 50px;
+  margin-right: 5px;
+  margin-left: 10px;
+}
+#big {
+  border-radius: 12px;
+  height: 70px;
+  width: 90px;
+  margin-right: 5px;
+  margin-left: 10px;
+}
+#block {
+  height: 40px;
+  width: 150px;
+  margin-left: 40px;
+}
+#type_select {
+  width: 250px;
+  margin-left: 10px;
+  margin-bottom: 50px;
+  margin-top: 5px;
+}
+button {
+  background-color: white;
+  margin-bottom: 10px;
+}
+input {
+  border-radius: 12px;
+}
+label {
+  margin-left: 10px;
+  margin-bottom: 10px;
+  margin-top: 5px;
+  font-weight: bold;
+}
+img {
+  height: 50%;
+  width: 40%;
+}
+</style>
--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,16 +1,40 @@
 <template>
-  <div v-if="appointment" class="appointment">
-    <h1>Afspraak:</h1>
-    <h3>afspraak nummer: {{ appointment.number }}</h3>
-    <h3>naam: {{ appointment.customer }}</h3>
-    <h3>email: {{ appointment.email }}</h3>
-    <h3>telefoonnummer: {{ appointment.phoneNumber }}</h3>
-    <h3>datum: {{ appointment.date }}</h3>
-    <h3>tijd: {{ appointment.time }}</h3>
-    <h3 v-if="appointment.doctor == 1">dokter: karel lant</h3>
-    <h3 v-if="appointment.doctor == 2">dokter: danique de beer</h3>
-    <p>we hopen u dan te zien (: </p>
+<h1 class="appointment_header">Afspraak maken</h1>
+<div class="test">
+  <h2 class="">hier komt de balk</h2>
+  <div v-if="appointment" class="appointment_form">
+    <h3>Afspraak bevestiging</h3>
+    <div class="row">
+      <div class="appointment_result">
+        <p>Afspraak nummer</p>
+        <p>Klant</p>
+        <p>Mobiel nummer</p>
+        <p>Email adres</p>
+        <p>Diersoort</p>
+        <p>Afspraak type</p>
+        <p>Datum</p>
+        <p>Tijd</p>
+        <p v-if="appointment.doctor == 1"></p>
+        <p v-if="appointment.doctor == 2"></p>
+      </div>
+      <div class="appointment_result">
+        <p>{{ appointment.number }}</p>
+        <p>{{ appointment.customer }}</p>
+        <p>{{ appointment.phoneNumber }}</p>
+        <p>{{ appointment.email }}</p>
+        <p>{{ appointment.type_animal }}</p>
+        <p>{{ appointment.type }}</p>
+        <p>{{ appointment.date }}</p>
+        <p>{{ appointment.time }}</p>
+        <p v-if="appointment.doctor == 1">dokter: karel lant</p>
+        <p v-if="appointment.doctor == 2">dokter: danique de beer</p>
+      </div>
+      <div class="appointment_result_pic">
+        <img src="../assets/dog-computer.jpg">
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
