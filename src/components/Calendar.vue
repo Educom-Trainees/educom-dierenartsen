01f253d4e7d2ae0f2012fc9192bb430add8ef682
Quincy Tromp <quincy.tromp@hotmail.com>
2023-11-10 12:47:29 +0100

    Added appointment overview functionality

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,8 +1,8 @@
 <template>
     <table id="overview-calendar" class="table table-bordered table-responsive">
         <tr>
-            <th id="clock-o"><i class="fa fa-clock-o" aria-hidden="true"></i></th>
-            <th>{{ doctor }}</th>
+            <th id="th-clock"><i class="fa fa-clock-o" aria-hidden="true"></i></th>
+            <th id="th-doc">{{ doctor }}</th>
         </tr>
         <tr v-for="item in calculatedTimeslots">
             <td class="event-time">{{ item.timeslot }}</td>
@@ -11,7 +11,7 @@
                     <span>{{ item.appointment.customer }} - {{ item.appointment.time }}</span>
                 </div>
             </td>
-            <td v-else class="no-event" rowspan="1"></td>
+            <td v-else-if="item.show" class="no-event" rowspan="1"></td>
         </tr>
     </table>
 </template>
@@ -19,24 +19,74 @@
 <script>
     export default {
         name: 'Calendar',
-        props: ['doctor', 'timeslots', 'appointments'],
+        props: ['doctor', 'doctorId', 'timeslots', 'appointments'],
         computed: {
             calculatedTimeslots() {
-                return this.timeslots.map(t => {
-                    const a = this.appointments.filter(a => a.time == t && a.doctor == this.doctor)
+                const array = this.timeslots.map(t => {
+                    const a = this.appointments.filter(a => a.time == t && a.doctor == this.doctorId)
                     return {
                         "timeslot": t,
-                        "appointment": a? a[0] : undefined
+                        "doctor": this.doctorId,
+                        "appointment": a? a[0] : undefined,
+                        "show": true
                     }
                 })
+                for (var i = 0; i < array.length; i++) { 
+                    if (array[i].appointment) {
+                        if (array[i].appointment.duration > 15) {
+                            const r = array[i].appointment.duration / 15
+                            for (var n = 1; n < r; n++) {
+                                array[i+n].show = false
+                            }
+                        }
+                    }
+                }
+                console.log(1, array)
+                return array
+                // return array.map(t => {
+                //     if (t.appointment && t.appointment.duration > 15) {
+                //         const rowspan = t.appointment.duration / 15
+                //         for (var i = 1; i < rowspan; i++) {
+                //             const x = new Date([t.appointment.date, t.appointment.time]
+                //                 .join(' '))
+                //                 .setMinutes(x.getMinutes() + i * 15)
+                //             const a = [String(x.getHours).padStart(2, '0'), String(x.getMinutes).padStart(2, '0')]
+                //                 .join(':')
+                            
+                //         }
+                //     }
+                // })
             }
         }
     }
 </script>
 
 <style>
+    .table #th-doc {
+        background-color: var(--lightGrey);
+    }
+    .table #th-clock i {
+        background-color: var(--darkGrey);
+    }
+    .table #th-clock {
+        text-align: center;
+        font-size: 2rem;
+        border-right: 1px solid var(--bs-border-color);
+        background-color: var(--darkGrey);
+    }
+    .table .event-time {
+        border: none;
+        border-right: 1px solid var(--bs-border-color);
+        width: 10%;
+        background-color: var(--darkGrey);
+        text-align: center;
+        padding: 2px;
+    }
     .appointment {
-        background-color: blue;
-        color: white;
+        background-color: blue !important;
+        color: white !important;
+    }
+    .col-lg-6 {
+        padding: 0;
     }
 </style>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -11,22 +11,14 @@
                     <button @click="nextDate()" type="button" class="btn btn-secondary active">&gt;</button>
                 </div>
             </div>
-            
-            <!-- <table id="overview-calendar" class="table table-bordered table-responsive">
-                <tr>
-                    <th id="clock-o"><i class="fa fa-clock-o" aria-hidden="true"></i></th>
-                    <th width="47%">{{ doctors[0] }}</th>
-                    <th width="47%">{{ doctors[1] }}</th>
-                </tr>
-                <tr v-for="timeslot in timeslots.filter(timeslot => timeslot.doctor == 1)">
-                    <td class="event-time">{{ timeslot.time }}</td>
-                    <td class="no-event" rowspan="1"></td>
-                    <td class="no-event" rowspan="1"></td>
-                </tr>
-            </table> -->
-
-            <Calendar doctor="Karel Lant" :timeslots="timeslots" :appointments="activeAppointments" />
-            <Calendar doctor="Danique de Beer" :timeslots="timeslots" :appointments="activeAppointments" />
+        </div>
+    </div>
+    <div class="row flex-column flex-lg-row">
+        <div class="col-12 col-lg-6">
+            <Calendar doctor='Karel Lant' doctorId="1" :timeslots="timeslots" :appointments="appointments" />
+        </div>
+        <div class="col-12 col-lg-6">
+            <Calendar doctor='Danique de Beer' doctorId="2" :timeslots="timeslots" :appointments="appointments" />
         </div>
     </div>
   </div>
@@ -36,16 +28,15 @@
     import axios from 'axios'
     import Calendar from '../components/Calendar.vue'
 
+    const testDate = '2023-11-10'
+    const today = new Date(testDate)
+    const baseUrlActiveAppointments = 'http://localhost:3000/appointments?status=0&date='
     const timeslots = [
         "09:00","09.15","09:30","09:45","10:00","10:15","10:30","10:45","11:00",
         "11:15","11:30","11:45","12:00","12:15","12:30","12:45","13:00","13:15",
         "13:30","13:45","14:00","14:15","14:30","14:45","15:00","15:15","15:30",
-        "15:45","16:00","16:15","16:30","16:45","17:00","17:15"]
-
-    const testDate = '2023-11-10'
-    const today = new Date(testDate)
-    const baseUrlTimeslots = 'http://localhost:3000/time-slots?date='
-    const baseUrlActiveAppointments = 'http://localhost:3000/appointments?status=0&date='
+        "15:45","16:00","16:15","16:30","16:45","17:00","17:15"
+    ]
 
     export default {
         name: 'Overview',
@@ -53,15 +44,14 @@
             Calendar
         },
         mounted() {
-            // this.getTimeslots(testDate)
-            this.getActiveAppointments(testDate)
+            this.getAppointments(this.toDateString(this.date))
         },
         data() {
             return {
-                timeslots: timeslots,
-                activeAppointments: [],
                 today: today,
                 date: new Date(testDate),
+                timeslots: timeslots,
+                appointments: [],
             }
         },
         methods: {
@@ -81,81 +71,33 @@
                 this.date.setDate(this.date.getDate() - 1)
                 console.log('previous day: ', this.date)
             }, 
-            // async getTimeslots(dateString) {
-            //     const url = baseUrlTimeslots + dateString
-            //     await axios.get(url)
-            //         .then(response => {
-            //             this.timeslots = response.data;
-            //         })
-            //         .catch(error => {
-            //                 console.error('Error getting timeslots:', error);
-            //     }); 
-            // },
-            async getActiveAppointments(dateString) {
+            getAppointments(dateString) {
                 const url = baseUrlActiveAppointments + dateString
-                await axios.get(url)
-                    .then(response => {
-                        this.activeAppointments = response.data;
-                    })
-                    .catch(error => {
-                            console.error('Error getting active appointments:', error);
-                }); 
+                axios.get(url)
+                    .then(response => this.appointments = response.data)
+                    .catch(error => console.log('Error getting appointments:', error))
             },
-            // mountAppointments() {
-            //     const appointmentsObject = {
-            //         1: this.activeAppointments.filter(a => a.doctor == 1),
-            //         2: this.activeAppointments.filter(a => a.doctor == 2)
-            //     }
-            //     const timeslotArray = [
-            //         this.timeslots.filter(t => t.doctor == 1),
-            //         this.timeslots.filter(t => t.doctor == 2)
-            //     ]
-            //     for (let doctorsKey in appointmentsObject) {
-            //         for (let appointment of appointmentsObject[doctorsKey]) {
-            //             for (var timeslotIndex = 0; timeslotIndex < timeslotArray.length; timeslotIndex++) {
-            //                 if (appointment.time == timeslotArray[timeslotIndex].time) {
-            //                     this.addToOverview(timeslotIndex, appointment) 
-            //                 }
-            //             }
-            //         }
-            //     }
-            // },
-            // calculateRowspan(duration) {
-            //     switch (duration) {
-            //         case "15": 
-            //             return "1"
-            //         case "30":
-            //             return "2"
-            //         case "45":
-            //             return "3"
-            //         case "60":
-            //             return "4"
-            //     }
-            // },
-            // addToOverview(timeslotIndex, appointment) {
-            //     const customer = appointment.customer
-            //     const startTime = appointment.time
-            //     const table = document.getElementById('overview-calendar').getElementsByTagName('tbody')[0]
-            //     const timeslotRow = table.rows[timeslotIndex]
-            //     const cell = timeslotRow.childNodes[appointment.doctor]
-
-            //     cell.classList.remove("no-event")
-            //     cell.classList.add("has-event")
-            //     const rowspan = this.calculateRowspan(appointment.duration)
-            //     cell.setAttribute("rowspan", rowspan)
-            //     cell.innerHTML = 
-            //         '<div class="appointment">' +
-            //             '<span>' + customer - startTime + '</span>' +
-            //         '</div>'
-            //     console.log('added to calendar')
-            // },
-        },  
+        },
+        // computed: {
+        //     calculatedTimeslots(doctor) {
+        //         return this.timeslots.map(t => {
+        //             const a = this.appointments.filter(a => a.time == t && a.doctor == doctor)
+        //             return {
+        //                 "timeslot": t,
+        //                 "appointment": a? a[0] : undefined
+        //             }
+        //         })
+        //     }
+        // }  
     }
 </script>
 
 <style>
     .container-fluid * {
         text-align: left;
+    }
+    .col-12 {
+        padding: 0;
     }
     #datepicker-area {
         min-height: 70px;
@@ -165,13 +107,5 @@
         background-color: var(--lightGrey);
         color: black;
         border: none;
-    }
-    #clock-o {
-        text-align: center;
-        font-size: 2rem;
-    }
-    .table .event-time {
-        text-align: center;
-        padding: 2px;
     }
 </style>
