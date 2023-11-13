1a781c2022f2628d4ab86d627193e8795a53c180
Quincy Tromp <quincy.tromp@hotmail.com>
2023-11-13 15:27:22 +0100

    Exported function

---
+++ src/composables/arrayTransfromer.js
@@ -0,0 +1,17 @@
+export function combineTimeslotAppointments(timeslots, appointments) {
+    const array = timeslots.map(t => {
+        const a = appointments.filter(a => a.time == t.timeslot)
+        return {...t, 'appointment': a? a[0] : undefined, 'show': true}
+    })
+    for (var i = 0; i < array.length; i++) { 
+        if (array[i].appointment) {
+            if (array[i].appointment.duration > 15) {
+                const r = array[i].appointment.duration / 15
+                for (var n = 1; n < r; n++) {
+                    array[i+n].show = false
+                }
+            }
+        }
+    }
+    return array
+}
