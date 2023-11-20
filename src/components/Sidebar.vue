bc8c92880613fa0d0f1423fc7a5b23a665a46791
Quincy Tromp <quincy.tromp@hotmail.com>
2023-11-20 10:28:41 +0100

    Updated sidebar on profile page

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="sidebar">
+        <div v-for="(item, index) in items" :key="index" @click="activate(index)" :class="{ 'active': selectedIndex === index }">
+          {{ item }}
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+    data() {
+        return {
+            showing: 'PersonalInfo',
+            items: ['Mijn Gegevens', 'Mijn Huisdieren', 'Mijn Afspraken'],
+            selectedIndex: 0,
+        }
+    },
+    methods: {
+        show(profileContent) {
+            this.showing = profileContent
+        },
+        activate(index) {
+            this.selectedIndex = index
+        }
+    }
+}
+</script>
+
+<style>
+.sidebar {
+    margin: 0;
+    padding: 0;
+    width: 250px;
+    background-color: var(--happyPaw3);
+    position: fixed;
+    height: 100%;
+    overflow: auto;
+}
+
+.sidebar div {
+    display: block;
+    color: white;
+    padding: 16px;
+    text-decoration: none;
+    font-size: 24px;
+}
+.sidebar div:hover {
+    cursor: pointer;
+}
+.sidebar div.active {
+    background-color: var(--happyPaw2);
+}
+
+div.content {
+    margin-left: 250px;
+}
+
+@media (max-width: 810px) {
+    .sidebar {
+        width: 100%;
+        height: auto;
+        position: relative;
+    }
+    .sidebar div {
+        display: block;
+        text-align: center;
+        cursor: pointer;
+    }
+    div.content {
+        margin-left: 0;
+    }
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,24 +1,36 @@
 <template>
     <TopNavigation />
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-3">
-                sidenav
-            </div>
-            <div class="col-9">
-                <h1>Profile</h1>
-            </div>
+        <Sidebar />
+        <div class="content">
+            <h2>Responsive Sidebar Example</h2>
+            <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+            <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+            <h3>Resize the browser window to see the effect.</h3>
         </div>
-    </div>
 </template>
 
 <script>
 import TopNavigation from '../components/TopNavigation.vue'
+import Sidebar from '../components/Sidebar.vue'
 
 export default {
     name: 'Profile',
     components: {
-        TopNavigation
+        TopNavigation,
+        Sidebar
+    },
+    data() {
+        return {
+            showing: 'PersonalInfo',
+        }
+    },
+    methods: {
+        show(profileContent) {
+            this.showing = profileContent
+        },
     }
 }
 </script>
+
+<style>
+</style>
