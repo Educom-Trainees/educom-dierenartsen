3280e0522ef7f9c8d3ff285c90d8e60fa5e73a76
Corby7 <corbijn.bulsink@hotmail.com>
2023-11-27 10:51:34 +0100

    reseeding appointments

---
+++ BackendASP/Migrations/20231127083512_ReseedAppointments.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReseedAppointments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Appointments",
+                columns: new[] { "Id", "AppointmentNumber", "Date", "Duration", "CustomerName", "PhoneNumber", "Email", "Preference", "Doctor", "Status", "PetTypeId", "AppointmentTypeId" },
+                values: new object[] { 1, 1, DateTime.Parse("2023-11-20"), 30, "Corbijn Bulsink", "0611330161", "corbijn.bullie@hoi.nl", 1, 1, 1, 4, 5 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
