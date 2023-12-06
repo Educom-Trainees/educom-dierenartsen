de9f2aa55ece40c7d043f31756a936a531659e47
Corby7 <corbijn.bulsink@hotmail.com>
2023-12-06 16:23:17 +0100

    modified get appointments to allow date, status or userid as parameter

--- frontentVue/src/composables/getAppointments.js
+++ frontentVue/src/composables/getAppointments.js
@@ -1,13 +1,27 @@
 import { ref } from 'vue'
 import { API_URL } from '../utils/api'
 
-const getAppointments = () => {
+const getAppointments = (date = null, status = null, userId = null) => {
     const appointments = ref(null)
     const error = ref(null)
 
     const load = async () => {
       try {
-        let data = await fetch(API_URL + 'appointments')
+        let url = API_URL + 'appointments'
+        // let data = await fetch(API_URL + 'appointments')
+
+        if (date !== null) {
+          url += `?date=${date}`
+        }
+        if (status !== null) {
+            url += `?status=${status}`
+        }
+        if (userId !== null) {
+            url += `?userId=${userId}`
+        }
+
+        let data = await fetch(url)
+        
         if(!data.ok){
           throw Error('no data found')
         }
