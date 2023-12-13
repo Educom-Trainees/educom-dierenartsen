d3c276ebac613a521f38b8a11658e757f39972af
Corby7 <corbijn.bulsink@hotmail.com>
2023-12-13 13:59:08 +0100

    made swaggerui always show up for now

--- BackendASP/Program.cs
+++ BackendASP/Program.cs
@@ -48,13 +48,15 @@ namespace Backend2
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+        /*    // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+*/
             app.UseHttpsRedirection();
 
             app.UseCors();
