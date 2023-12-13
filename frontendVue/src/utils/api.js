d14985aafb0a1db324a8c60b4a09134324eef143
Corby7 <corbijn.bulsink@hotmail.com>
2023-12-13 16:23:18 +0100

    added new api url for azure backend

--- frontendVue/src/utils/api.js
+++ frontendVue/src/utils/api.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://localhost:7027/" // Use APS.NET Backend
+export const API_URL = "https://happypaws-backendasp.azurewebsites.net/" // Use Azure Backend
+//export const API_URL = "https://localhost:7027/" // Use APS.NET Backend
 //export const API_URL = "http://localhost:3000/" // use JSON Server Backend
