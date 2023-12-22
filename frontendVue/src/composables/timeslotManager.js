3bcc73df6f3e2083a343a2a3bf6a4361df652737
Corby7 <corbijn.bulsink@hotmail.com>
2023-12-22 17:03:23 +0100

    modified function

--- frontendVue/src/composables/timeslotManager.js
+++ frontendVue/src/composables/timeslotManager.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { API_URL } from '../utils/api'
+import { toDateString } from './datetime-utils'
 
 
 const baseUrlTimeslots = API_URL + 'time-slots'
 const baseUrlTimeslotsByDate = baseUrlTimeslots + '?date='
 
-export async function GetAllTimeslots() { // This function gets todays timeslots
+export async function getAllTimeslots() { // This function gets todays timeslots
     try {
         const response = await axios.get(baseUrlTimeslots)
         if (Array.isArray(response.data) && response.data.length === 0) {
@@ -20,8 +21,8 @@ export async function GetAllTimeslots() { // This function gets todays timeslots
     }
 }
 
-export async function GetTimeslotsByDate(date) {
-    const url = baseUrlTimeslotsByDate + date
+export async function getTimeslotsByDate(date) {
+    const url = baseUrlTimeslotsByDate + toDateString(date)
     try {
         const response = await axios.get(url)
         if (Array.isArray(response.data) && response.data.length === 0) {
