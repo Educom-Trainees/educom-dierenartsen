7441941b882ce6dfdeff88a065d1b8f9965c53a8
Corby7 <corbijn.bulsink@hotmail.com>
2023-12-22 17:03:37 +0100

    commented out unused code

--- frontendVue/src/composables/getTime_slots.js
+++ frontendVue/src/composables/getTime_slots.js
@@ -1,26 +1,26 @@
-import { ref } from 'vue'
-import { API_URL } from '../utils/api'
-import { toDateString } from './datetime-utils'
+// import { ref } from 'vue'
+// import { API_URL } from '../utils/api'
+// import { toDateString } from './datetime-utils'
 
-const getTime_slots = (date) => {
-    const time_slots = ref(null)
-    const error = ref(null)
+// const getTime_slots = (date) => {
+//     const time_slots = ref(null)
+//     const error = ref(null)
 
-    const load = async () => {
-      try {
-        let data = await fetch(API_URL + 'time-slots?date=' + toDateString(date))
-        if(!data.ok){
-          throw Error('no data found')
-        }
-        time_slots.value = await data.json()
-      }
-      catch (err) {
-        error.value = err.message
-        console.log(error.value)
-      }
-    }
+//     const load = async () => {
+//       try {
+//         let data = await fetch(API_URL + 'time-slots?date=' + toDateString(date))
+//         if(!data.ok){
+//           throw Error('no data found')
+//         }
+//         time_slots.value = await data.json()
+//       }
+//       catch (err) {
+//         error.value = err.message
+//         console.log(error.value)
+//       }
+//     }
 
-    return { time_slots, error, load}
-}
+//     return { time_slots, error, load}
+// }
 
-export default getTime_slots
+// export default getTime_slots
