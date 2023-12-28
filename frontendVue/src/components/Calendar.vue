793a9d439b3fde4c66098bfee3473cd9d3e13e53
Laura Bokkers <L.A.Bokkers@gmail.com>
2023-12-28 20:14:47 +0100

    fixed bugs

--- frontendVue/src/components/Calendar.vue
+++ frontendVue/src/components/Calendar.vue
@@ -1,162 +1,193 @@
 <template>
-    <table id="overview-calendar" class="table table-bordered table-responsive h-100">
-        <tr>
-            <th id="th-clock">
-                <i class="fa fa-clock-o" aria-hidden="true"></i>
-            </th>
-            <th id="th-doc">
-                <span id="doctor">{{ doctor }}</span>
-                <span id="nr-appointment">{{ appointments.filter(a => a.doctor == doctorId || a.doctor == 3).length }}</span>
-            </th>
-        </tr>
-        <tr v-for="item in calculatedTimeslots">
-            <td class="timeslot">{{ item.time }}</td>
-            <td v-if="item.appointment" class="has-event" :rowspan="item.appointment.duration/15">
-                <div class="dropdown">
-                    <button class="btn btn-primary" 
-                    type="button" id="dropdownMenuButton" data-toggle="dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                    :style="{ 'height': (25 * (item.appointment.duration/15)) + 'px', 'background-color': color + '!important' }">
-                        <span class="appointment-info">{{ item.appointment.customer }} &mdash; {{ item.appointment.time }} - {{ 
-                        calculateEndTime(item.appointment.date,item.appointment.time,item.appointment.duration) }}</span>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <AppointmentDetail :appointment="item.appointment" />
-                    </div>
-                </div>
-            </td>
-            <td v-else-if="item.show" class="no-event" rowspan="1"></td>
-        </tr>
-    </table>
+  <table
+    id="overview-calendar"
+    class="table table-bordered table-responsive h-100"
+  >
+    <tr>
+      <th id="th-clock">
+        <i class="fa fa-clock-o" aria-hidden="true"></i>
+      </th>
+      <th id="th-doc">
+        <span id="doctor">{{ doctor }}</span>
+        <span id="nr-appointment">{{
+          appointments.filter((a) => a.doctor == doctorId || a.doctor == 3)
+            .length
+        }}</span>
+      </th>
+    </tr>
+    <tr v-for="item in calculatedTimeslots">
+      <td class="timeslot">{{ item.time }}</td>
+      <td
+        v-if="item.appointment"
+        class="has-event"
+        :rowspan="item.appointment.duration / 15"
+      >
+        <div class="dropdown">
+          <button
+            class="btn btn-primary"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+            :style="{
+              height: 25 * (item.appointment.duration / 15) + 'px',
+              'background-color': color + '!important',
+            }"
+          >
+            <span class="appointment-info"
+              >{{ item.appointment.customer }} &mdash;
+              {{ item.appointment.time }} -
+              {{
+                calculateEndTime(
+                  item.appointment.date,
+                  item.appointment.time,
+                  item.appointment.duration
+                )
+              }}</span
+            >
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <AppointmentDetail :appointment="item.appointment" />
+          </div>
+        </div>
+      </td>
+      <td v-else-if="item.show" class="no-event" rowspan="1"></td>
+    </tr>
+  </table>
 </template>
 
 <script>
-import AppointmentDetail from './AppointmentDetail.vue'
-import { TIMESLOTS } from '../utils/timeSlots.js'
-import { calculateEndTime } from '../composables/datetime-utils.js'
-import { combineTimeslotAppointments } from '../composables/arrayTransfromer.js'
+import AppointmentDetail from "./AppointmentDetail.vue";
+import { TIMESLOTS } from "../utils/timeSlots.js";
+import { calculateEndTime } from "../composables/datetime-utils.js";
+import { combineTimeslotAppointments } from "../composables/arrayTransfromer.js";
 import { GetTimeslotsByDate } from "../composables/timeslotManager.js";
 
 export default {
-    name: 'Calendar',
-    components: {
-        AppointmentDetail
-    },
-    props: ['doctor', 'doctorId', 'appointments', 'date', 'color'],
-    data() {
-        return {
-            tslot: TIMESLOTS.map(t => { return {'time': t, 'doctor': this.doctorId }}),
-            calculateEndTime: calculateEndTime,
-            availableTimeslots: [],
-            unavailableTimeslots: [],
-            vacationTimeslots: [],
-        }
-    },
-    methods: {
-        // async sortTimeslotAvailability() {
-        //     try {
-        //         // Transform date to correct format for backend (YYYY-MM-DD)
-        //         const year = this.date.getFullYear();
-        //         const month = String(this.date.getMonth() + 1).padStart(2, "0");
-        //         const day = String(this.date.getDate()).padStart(2, "0");
-        //         const formattedDate = `${year}-${month}-${day}`;
+  name: "Calendar",
+  components: {
+    AppointmentDetail,
+  },
+  props: ["doctor", "doctorId", "appointments", "date", "color"],
+  data() {
+    return {
+      tslot: TIMESLOTS.map((t) => {
+        return { time: t, doctor: this.doctorId };
+      }),
+      calculateEndTime: calculateEndTime,
+      availableTimeslots: [],
+      unavailableTimeslots: [],
+      vacationTimeslots: [],
+    };
+  },
+  methods: {
+    async sortTimeslotAvailability() {
+      try {
+        // Transform date to correct format for backend (YYYY-MM-DD)
+        const year = this.date.getFullYear();
+        const month = String(this.date.getMonth() + 1).padStart(2, "0");
+        const day = String(this.date.getDate()).padStart(2, "0");
+        const formattedDate = `${year}-${month}-${day}`;
 
-        //         const allTimeslots = await GetTimeslotsByDate(this.date)
-        //         console.log(allTimeslots);
-        //     } catch (error) {
-        //         console.error('Error fetching timeslots: ', error);
-        //     }
-        // }
-
+        const allTimeslots = await GetTimeslotsByDate(this.date);
+        console.log(allTimeslots);
+      } catch (error) {
+        console.error("Error fetching timeslots: ", error);
+      }
     },
-    watch: {
-        date: {
-            immediate: true,
-            handler(newValue) {
-                this.sortTimeslotAvailability();
-                console.log('New value of date prop:', newValue);
-            },
-        },
+  },
+  watch: {
+    date: {
+      immediate: true,
+      handler(newValue) {
+        this.sortTimeslotAvailability();
+        console.log("New value of date prop:", newValue);
+      },
     },
-    computed: {
-        calculatedTimeslots() {
-            const app = this.appointments.filter(a => (a.doctor == this.doctorId || a.doctor == 3))
-            return combineTimeslotAppointments(this.tslot, app)
-
-        }
-    }
-}
+  },
+  computed: {
+    calculatedTimeslots() {
+      const app = this.appointments.filter(
+        (a) => a.doctor == this.doctorId || a.doctor == 3
+      );
+      return combineTimeslotAppointments(this.tslot, app);
+    },
+  },
+};
 </script>
 
 <style>
 #overview-calendar {
-    margin-bottom: 0 !important;
+  margin-bottom: 0 !important;
 }
 .table #th-doc {
-    background-color: var(--lightGrey);
-    padding: 0 20px;
+  background-color: var(--lightGrey);
+  padding: 0 20px;
 }
 .table #th-clock i {
-    background-color: var(--darkGrey);
+  background-color: var(--darkGrey);
 }
 .table #th-clock {
-    text-align: center;
-    font-size: 2rem;
-    border-right: 1px solid var(--bs-border-color);
-    background-color: var(--darkGrey);
+  text-align: center;
+  font-size: 2rem;
+  border-right: 1px solid var(--bs-border-color);
+  background-color: var(--darkGrey);
 }
 .table th #doctor {
-    background-color: var(--lightGrey);
-    float: left;
-    padding: 8px;
+  background-color: var(--lightGrey);
+  float: left;
+  padding: 8px;
 }
 .table th #nr-appointment {
-    float: right;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    padding: 0;
-    min-width: 40px;
-    min-height: 40px;
-    background-color: var(--happyPaw1);
-    color: white;
+  float: right;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  padding: 0;
+  min-width: 40px;
+  min-height: 40px;
+  background-color: var(--happyPaw1);
+  color: white;
 }
 .table .timeslot {
-    border: none;
-    border-right: 1px solid var(--bs-border-color);
-    width: 10%;
-    background-color: var(--darkGrey);
-    text-align: center;
-    padding: 2px;
+  border: none;
+  border-right: 1px solid var(--bs-border-color);
+  width: 10%;
+  background-color: var(--darkGrey);
+  text-align: center;
+  padding: 2px;
 }
 .table .has-event .dropdown {
-    padding: 0;
+  padding: 0;
 }
 .table .has-event .dropdown-menu {
-    padding: 0;
+  padding: 0;
 }
 .table .has-event .btn {
-    color: black !important;
-    padding: 0 16px !important;
-    margin: 0 6px;
-    border-radius: 20px;
-    min-height: 25px;
-    border: none;
-    width: 93%;
+  color: black !important;
+  padding: 0 16px !important;
+  margin: 0 6px;
+  border-radius: 20px;
+  min-height: 25px;
+  border: none;
+  width: 93%;
 }
 .table .appointment-info {
-    padding: 8px;
+  padding: 8px;
 }
 #h-2 {
-    min-height: 50px;
+  min-height: 50px;
 }
 #h-3 {
-    min-height: 75px;
+  min-height: 75px;
 }
 #h-2 {
-    min-height: px;
+  min-height: px;
 }
 .col-lg-6 {
-    padding: 0;
+  padding: 0;
 }
 </style>
