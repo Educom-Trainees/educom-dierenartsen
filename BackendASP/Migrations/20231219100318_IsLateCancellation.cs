1a7fa8868292684c14e7e4bf8dab8caed22a303c
Corby7 <corbijn.bulsink@hotmail.com>
2024-01-03 10:46:01 +0100

    added islatecancellation

---
+++ BackendASP/Migrations/20231219100318_IsLateCancellation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsLateCancellation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLateCancellation",
+                table: "Appointments",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsLateCancellation",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLateCancellation",
+                table: "Appointments");
+        }
+    }
+}
