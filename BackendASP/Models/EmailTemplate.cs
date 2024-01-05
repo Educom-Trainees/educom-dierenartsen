5fffb121fa4656ffff812fe34ca84e9d79288ae4
Corby7 <corbijn.bulsink@hotmail.com>
2024-01-05 17:14:50 +0100

    created emailtemplate model

---
+++ BackendASP/Models/EmailTemplate.cs
@@ -0,0 +1,13 @@
+﻿using BackendASP.Models.Enums;
+
+namespace BackendASP.Models
+{
+    public class EmailTemplate
+    {
+        public int Id { get; set; }
+        public required string TemplateName { get; set; }
+        public required string Subject { get; set; }
+        public required string Body { get; set; }
+        public EmailTypes EmailType { get; set; }
+    }
+}
