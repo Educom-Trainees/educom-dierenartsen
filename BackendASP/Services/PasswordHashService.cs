a5991a6e860d455c3b5ec63de76c16f873145885
Corby7 <corbijn.bulsink@hotmail.com>
2024-01-19 15:56:14 +0100

    created passwordhashservice

---
+++ BackendASP/Services/PasswordHashService.cs
@@ -0,0 +1,16 @@
+﻿using BackendASP.Models;
+using Microsoft.AspNetCore.Identity;
+
+public interface IPasswordHasherService
+{
+    string HashPassword(string password);
+}
+
+public class PasswordHasherService : IPasswordHasherService
+{
+    public string HashPassword(string password)
+    {
+        // Use the PasswordHasher<User> internally
+        return new PasswordHasher<User>().HashPassword(null, password);
+    }
+}
