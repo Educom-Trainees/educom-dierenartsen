a9920d5a7314876f5ec2a6d49a2cdc0808ea42b2
Corby7 <corbijn.bulsink@hotmail.com>
2024-01-19 15:43:52 +0100

    removed commented out code

--- frontendVue/src/composables/getAppointment_types.js
+++ frontendVue/src/composables/getAppointment_types.js
@@ -1,31 +1,3 @@
-// import { ref } from 'vue'
-// import { API_URL } from '../utils/api'
-
-// const getAppointment_types = () => {
-//     const appointment_types = ref(null)
-//     const error = ref(null)
-
-//     const load = async () => {
-//       try {
-//         let data = await fetch(API_URL + 'appointment-types')
-//         if(!data.ok){
-//           throw Error('no data found')
-//         }
-//         appointment_types.value = await data.json()
-//         console.log(appointment_types)
-//         console.log(appointment_types.value)
-//       }
-//       catch (err) {
-//         error.value = err.message
-//         console.log(error.value)
-//       }
-//     }
-//     load()
-//     return { appointment_types, error}
-// }
-
-// export default getAppointment_types
-
 import { ref } from 'vue'
 import axios from 'axios'
 import { API_URL } from '../utils/api'
