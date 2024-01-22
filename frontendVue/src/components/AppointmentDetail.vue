6111ce65f43e8efdc399990921a8c03738c45dcb
Laura Bokkers <L.A.Bokkers@gmail.com>
2024-01-22 12:28:46 +0100

    no show button is now visible for future dates, but disabled. + uncommented code (you can't go back to the past in the agenda)

--- frontendVue/src/components/AppointmentDetail.vue
+++ frontendVue/src/components/AppointmentDetail.vue
@@ -81,9 +81,11 @@
         <div class="row">
           <div class="col-sm">
             <button
-              v-if="new Date(appointment.date) < new Date()"
               @click="registerNoShowAppointment(appointment)"
-              :disabled="appointment.lateStatus === 1"
+              :disabled="
+                appointment.lateStatus === 1 ||
+                new Date(appointment.date) > new Date()
+              "
               class="btn btn-action action-move"
             >
               No Show
