a2692931fee4ce5684808cf187c65ccf70f53fe1
Laura Bokkers <L.A.Bokkers@gmail.com>
2024-01-23 15:19:21 +0100

    api change

--- frontendVue/src/utils/api.js
+++ frontendVue/src/utils/api.js
@@ -1,3 +1,3 @@
-//export const API_URL = "https://happypaws-backendasp.azurewebsites.net/api/" // Use Azure Backend
-export const API_URL = "https://localhost:7027/api/" // Use APS.NET Backend
+export const API_URL = "https://happypaws-backendasp.azurewebsites.net/api/" // Use Azure Backend
+// export const API_URL = "https://localhost:7027/api/" // Use APS.NET Backend
 //export const API_URL = "http://localhost:3000/" // use JSON Server Backend
