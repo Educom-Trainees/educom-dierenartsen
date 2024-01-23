312999a63141981d6df54cd0dea96fa0fb1cb04a
Corby7 <corbijn.bulsink@hotmail.com>
2024-01-23 09:45:48 +0100

    added auto logout on expired token

--- frontendVue/src/main.js
+++ frontendVue/src/main.js
@@ -2,7 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { logoutUser } from './composables/accountManager'
 
 axios.defaults.withCredentials = true;
+
+axios.interceptors.response.use(
+    async (response) => {
+      return response;
+    },
+    async (error) => {
+      if (error.response && error.response.status === 401) {
+        await logoutUser();
+        router.push('/login');
+        alert('U bent uitgelogd omdat uw sessie is verlopen.');
+      }
+  
+      return Promise.reject(error);
+    }
+  );
 
 createApp(App).use(router).mount('#app')
