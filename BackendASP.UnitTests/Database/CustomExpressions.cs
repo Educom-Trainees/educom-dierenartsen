347c2b31d073b332e13ea7ea3fb8b71ccdf0915c
JeroenHeemskerk <j.w.heemskerk@gmail.com>
2024-01-24 12:36:50 +0100

    Removed 09:00 timeslot, simplified Property("Avaliable") to Avaliable() Added MarkBooked functions, added some tests

---
+++ BackendASP.UnitTests/Database/CustomExpressions.cs
@@ -0,0 +1,19 @@
+﻿using BackendASP.Models.DTO;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendASP.UnitTests.Database
+{
+    internal static class CustomExpressions
+    {
+        public static ResolvableConstraintExpression Available(this ConstraintExpression constraint)
+        {
+            return constraint.Property(nameof(TimeSlotDTO.Available));
+        }
+
+    }
+}
--- BackendASP.UnitTests/Database/TimeSlotControllerTests.cs
+++ BackendASP.UnitTests/Database/TimeSlotControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using BackendASP.Controllers;
 using BackendASP.Models.DTO;
 using BackendASP.Models.Enums;
+using NUnit.Framework.Constraints;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,54 +21,98 @@ namespace BackendASP.UnitTests.Database
         public void TestGetEmptyListOfTimeSlotDTOs()
         {
             // Prepare
-            TimeSlotBuilder tsb = new TimeSlotBuilder();
+            TimeSlotBuilder tsb = new TimeSlotBuilder(false);
             List<TimeSlotDTO> start = tsb.GetSlots();
 
             // Run
             var result = TimeSlotsController.CalculateBreak(start);
 
             // Validate
-            Assert.That(result, Has.Count.EqualTo(22));
-            Assert.That(result, Has.None.Property(nameof(TimeSlotDTO.Available)).EqualTo(SlotAvailable.BREAK));
-            Assert.That(result, Has.All.Property(nameof(TimeSlotDTO.Available)).EqualTo(SlotAvailable.AVAILABLE_45));
+            Assert.That(result, Has.Count.EqualTo(20));
+            Assert.That(result, Has.None.Available().EqualTo(SlotAvailable.BREAK));
+            Assert.That(result, Has.All.Available().EqualTo(SlotAvailable.AVAILABLE_45));
         }
 
         [Test]
         public void TestBookedOutsideWindow()
         {
             // Prepare
-            TimeSlotBuilder tsb = new TimeSlotBuilder();
-            tsb.SetAvailable(EIndex.K_09_45, SlotAvailable.BOOKED);
-            tsb.SetAvailable(EIndex.K_09_30, SlotAvailable.AVAILABLE_15);
-            tsb.SetAvailable(EIndex.K_09_15, SlotAvailable.AVAILABLE_30);
+            TimeSlotBuilder tsb = new TimeSlotBuilder(false);
+            tsb.MarkBooked(EIndex.K_09_45); // Mark 9:45 - 10:00 booked for Karel
             List<TimeSlotDTO> start = tsb.GetSlots();
 
             // Run
             var result = TimeSlotsController.CalculateBreak(start);
 
             // Validate
-            Assert.That(result, Has.Count.EqualTo(22));
-            Assert.That(result, Has.None.Property(nameof(TimeSlotDTO.Available)).EqualTo(SlotAvailable.BREAK));
-            Assert.That(result.Where(s =>s.Doctor == DoctorTypes.KAREL_LANT && InTimeWindow(s)), Has.All.Property(nameof(TimeSlotDTO.Available)).EqualTo(SlotAvailable.AVAILABLE_45));
+            Assert.That(result, Has.Count.EqualTo(20));
+            Assert.That(result, Has.None.Available().EqualTo(SlotAvailable.BREAK));
+            Assert.That(result.Where(s =>s.Doctor == DoctorTypes.KAREL_LANT && InTimeWindow(s)), Has.All.Available().EqualTo(SlotAvailable.AVAILABLE_45));
         }
 
         [Test]
-        public void TestBookedInsideWindow()
+        public void TestBookedInsideWindow_NoBreakNeededYet()
         {
             // Prepare
-            TimeSlotBuilder tsb = new TimeSlotBuilder();
-            tsb.SetAvailable(EIndex.K_10_00, SlotAvailable.BOOKED);
-            tsb.SetAvailable(EIndex.K_09_45, SlotAvailable.AVAILABLE_15);
-            tsb.SetAvailable(EIndex.K_09_30, SlotAvailable.AVAILABLE_30);
+            TimeSlotBuilder tsb = new TimeSlotBuilder(false);
+            tsb.MarkBooked(EIndex.K_10_00, EIndex.K_10_15); // Mark 10:00 - 10:30 booked for Karel
             List<TimeSlotDTO> start = tsb.GetSlots();
+            // prepare expected result
+            tsb.SetAvailable(EIndex.K_10_15, SlotAvailable.AVAILABLE_30); // Expect 10:15 to 10:45 to be restricted, to prevent a booking from 10:15 to 11:00
+            List<TimeSlotDTO> expected = tsb.GetSlots();
 
             // Run
             var result = TimeSlotsController.CalculateBreak(start);
 
             // Validate
-            Assert.That(result, Has.Count.EqualTo(22));
-            Assert.That(result, Has.None.Property(nameof(TimeSlotDTO.Available)).EqualTo(SlotAvailable.BREAK));
-            Assert.That(result, Has.ItemAt(EIndex.K_10_15).Property(nameof(TimeSlotDTO.Available)).EqualTo(SlotAvailable.AVAILABLE_30));
+            Assert.That(result, Has.Count.EqualTo(20));
+            Assert.That(result, Has.None.Available().EqualTo(SlotAvailable.BREAK));
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestKarelNeedsABreakWithDanique()
+        {
+            // Prepare
+            TimeSlotBuilder tsb = new TimeSlotBuilder(false);
+            tsb.MarkBooked(EIndex.K_10_00, EIndex.K_10_30); // Mark 10:00 - 10:30 booked for Karel
+            tsb.MarkBooked(EIndex.K_10_45, EIndex.K_11_30); // Mark 10:45 - 11:15 booked for Karel
+            List<TimeSlotDTO> start = tsb.GetSlots();
+            // prepare expected result
+            tsb.SetAvailable(EIndex.K_10_30, SlotAvailable.BREAK);
+            tsb.SetAvailable(EIndex.D_10_30, SlotAvailable.BREAK);
+            List<TimeSlotDTO> expected = tsb.GetSlots();
+
+            // Run
+            var result = TimeSlotsController.CalculateBreak(start);
+
+            // Validate
+            Assert.That(result, Has.Count.EqualTo(20));
+            Assert.That(result, Has.Exactly(2).Available().EqualTo(SlotAvailable.BREAK));
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestBothDoctorsGetABreak()
+        {
+            // Prepare
+            TimeSlotBuilder tsb = new TimeSlotBuilder(false);
+            tsb.MarkBooked(EIndex.K_10_00, EIndex.K_10_15); // Mark 10:00 - 10:15 booked for Karel
+            tsb.MarkBooked(EIndex.K_10_30, EIndex.K_10_45); // Mark 10:30 - 10:45 booked for Karel
+            tsb.MarkBooked(EIndex.D_10_45, EIndex.D_11_30); // Mark 10:45 - 11:15 booked for Danique
+            List<TimeSlotDTO> start = tsb.GetSlots();
+            // prepare expected result
+            tsb.SetAvailable(EIndex.K_10_15, SlotAvailable.BREAK);
+            tsb.SetAvailable(EIndex.D_10_15, SlotAvailable.BREAK);
+            List<TimeSlotDTO> expected = tsb.GetSlots();
+
+            // Run
+            var result = TimeSlotsController.CalculateBreak(start);
+
+            // Validate
+            Assert.That(result, Has.Count.EqualTo(20));
+            Assert.That(result, Has.Exactly(2).Available().EqualTo(SlotAvailable.BREAK));
+            Assert.That(result, Is.EqualTo(expected));
         }
 
     }
