1abecaad1704b56ddbfcd169a5de84ac23c09c44
JeroenHeemskerk <j.w.heemskerk@gmail.com>
2024-01-24 12:35:00 +0100

    Added ToString and Equals functions

--- BackendASP/Models/DTO/TimeSlotDTO.cs
+++ BackendASP/Models/DTO/TimeSlotDTO.cs
@@ -8,5 +8,24 @@ namespace BackendASP.Models.DTO
         public required string Time { get; set; }
         public DoctorTypes Doctor { get; set; }
         public SlotAvailable Available { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id,2}|{Doctor.ToString().First()}_{Time}|{Available}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            TimeSlotDTO? other = obj as TimeSlotDTO;
+            if (other == null)
+            {
+                return false;
+            }
+            return other.Id == Id && other.Time == Time && other.Doctor == Doctor && other.Available == Available;
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() + Time.GetHashCode() + Doctor.GetHashCode() + Available.GetHashCode();
+        }
     }
 }
