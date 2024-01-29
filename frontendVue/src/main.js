e452479aaa246bdee5f9991a40d1af7aa8a0a66f
LauraAnneB <88866166+LauraAnneB@users.noreply.github.com>
2024-01-29 16:43:42 +0100

    implemented start- and endtime to work-schedule functionality, using the Vue Datepicker library

--- frontendVue/src/main.js
+++ frontendVue/src/main.js
@@ -4,21 +4,24 @@ import router from './router'
 import axios from 'axios'
 import { logoutUser } from './composables/accountManager'
 
+
 axios.defaults.withCredentials = true;
 
 axios.interceptors.response.use(
-    async (response) => {
-      return response;
-    },
-    async (error) => {
-      if (error.response && error.response.status === 401) {
-        await logoutUser();
-        router.push('/login');
-        alert('U bent uitgelogd omdat uw sessie is verlopen.');
-      }
-  
-      return Promise.reject(error);
+  async (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      await logoutUser();
+      router.push('/login');
+      alert('U bent uitgelogd omdat uw sessie is verlopen.');
     }
-  );
 
+    return Promise.reject(error);
+  }
+);
 createApp(App).use(router).mount('#app')
+
+
+
