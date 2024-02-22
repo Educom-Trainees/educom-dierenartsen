b1f46885146efd95222a4c42b7baf3fad30af5d4
JeroenHeemskerk <j.w.heemskerk@gmail.com>
2024-02-22 18:04:51 +0100

    Added ToString to available days

--- BackendASP/Models/AvailableDays.cs
+++ BackendASP/Models/AvailableDays.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text;
+using System.Text.Json.Serialization;
 
 namespace BackendASP.Models
 {
@@ -11,6 +12,19 @@ namespace BackendASP.Models
 
         public DateOnly StartDate { get; set; }
         public DateOnly? EndDate { get; set;}
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder($"AD[{StartDate.ToShortDateString()}-{(EndDate == null ? "no end yet." : EndDate.Value.ToShortDateString())}] ");
+            if ((Days & (1 << (int)DayOfWeek.Sunday)) != 0) { sb.Append("Sun, "); }
+            if ((Days & (1 << (int)DayOfWeek.Monday)) != 0) { sb.Append("Mon,"); }
+            if ((Days & (1 << (int)DayOfWeek.Tuesday)) != 0) { sb.Append("Tue,"); }
+            if ((Days & (1 << (int)DayOfWeek.Wednesday)) != 0) { sb.Append("Wed,"); }
+            if ((Days & (1 << (int)DayOfWeek.Thursday)) != 0) { sb.Append("Thu,"); }
+            if ((Days & (1 << (int)DayOfWeek.Friday)) != 0) { sb.Append("Fri,"); }
+            if ((Days & (1 << (int)DayOfWeek.Saturday)) != 0) { sb.Append("Sat,"); }
+            return sb.ToString();
+        }
     }
 }
         
